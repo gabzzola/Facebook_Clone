34d688ed1ea008f359a0d6943f286ee612ee942c
gabzzola <gazzola.co@gmail.com>
2024-10-03 00:19:41 -0300

    Construi o layout para o mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'gray-main': '#465A69',
+        'blue-main': '#1877F2',
+      },
+    },
   },
   plugins: [],
 }
