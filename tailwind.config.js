16a7289e57a95bd6defb588ea9e1f97c62db87f4
gabzzola <gazzola.co@gmail.com>
2024-11-17 22:03:34 -0300

    Finalizando a interface para desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'gray-main': '#465A69',
         'blue-main': '#1877F2',
       },
+      spacing: {
+        '290px': '290px',
+        '980px': '980px'
+      }
     },
   },
   plugins: [],
